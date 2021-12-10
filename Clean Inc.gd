2ee0f16577640c96a40edf05d372b7c95011b363
GalaXy <neamtumatei08@gmail.com>
2021-12-10 22:11:55 +0200

    a

--- Clean Inc.gd
+++ Clean Inc.gd
@@ -1,4 +1,22 @@
 extends Node2D
 
-onready var personaj_shape = get_node("fundal/personaj/personaj_shape")
+var spid = 99
+var velocity = Vector2()
 
+func _ready():
+	random()
+	
+func _process(delta):
+	if Input.is_key_pressed(KEY_E):
+		velocity = Vector2(spid, 0)
+	else:
+		velocity = Vector2(0,0)
+	
+	
+
+func random():
+	var rngvar = RandomNumberGenerator.new()
+	var posx
+	rngvar.randomize()
+	posx = rngvar.randi_range(150, 850)
+	$fundal/gunoi_sprite.position = Vector2(posx, 550)
